63b493176b54e91af2ee94129c3f1caad21e71bc
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-01-27 16:33:32 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
+public class odd
+{	
+	public static void main(String[] args)
+	{
+		Scanner s= new Scanner(System.in);
+		System.out.println("input: ");
+		int i = s.nextInt();
+		int n = s.nextInt();
+		System.out.println("output:");
+		for(;i < n; i++)
+		   {
+			if( i % 2 != 0)
+			  System.out.print(i + " ");
+                   }
+		
+	}
+}
