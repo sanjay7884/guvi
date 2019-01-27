6e3dbf805a4b52ff39291bd23e06daa1ee5f2a13
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-01-27 16:31:45 +0530

    Update and rename evenintervel.java to even.java

--- even.java
+++ even.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 import java.io.*;
-public class evenintervel
+public class even
 {	
 	public static void main(String[] args)
 	{
 		Scanner s= new Scanner(System.in);
 		System.out.println("input: ");
+		int i = s.nextInt();
 		int n = s.nextInt();
 		System.out.println("output:");
-		for(int i = 2; i <= n; i = i + 2)
-		System.out.print(i+" ");
+		for(;i < n; i++)
+		   {
+			if( i % 2 == 0)
+			  System.out.print(i + " ");
+                   }
+		
 	}
 }
